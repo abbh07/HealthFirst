6e8750adb79edf65ff863e03c3d737ceef97a971
Aakash Bhattacharya <aakashbhattacharya07@gmail.com>
2016-04-13 19:43:34 +0530

    Git Initialized

---
+++ src/regression/Test.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regression;
+
+import matrix.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author aakash
+ */
+public class Test {
+    double x[][];
+    double y[][];
+
+    public Test() {
+        this.x = new double[][]{{4,0,1}, {7,1,1}, {6,1,0}, {2,0,0}, {3,0,1}};
+        this.y = new double[][]{{27},{29},{23},{20},{21}};
+    }
+    
+    public void Calculate() throws NoSquareException{
+        Matrix X = new Matrix(x);
+        Matrix Y = new Matrix(y);
+        MultiLinear ml = new MultiLinear(X,Y);
+        Matrix beta = ml.calculate();
+        for(int i=0;i<beta.getNrows();i++)
+        {
+            for(int j=0;j<beta.getNcols();j++)
+            {
+                System.out.println(beta.getValueAt(i, j));
+            }
+            System.out.println();
+        }
+        for (int i=0;i<Y.getNrows();i++) {
+            double predictedY =  beta.getValueAt(0, 0);
+            for (int j=1; j< beta.getNrows(); j++) {
+                predictedY += beta.getValueAt(j, 0) * X.getValueAt(i, j-1);
+            }
+            System.out.println(Y.getValueAt(i, 0) + " -> " + predictedY);
+        }
+    }
+    
+}
+
+class BMI{    
+    public static void main(String args[]) throws NoSquareException{
+        Test t = new Test();
+        t.Calculate();
+        
+        
+
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+	} catch (ClassNotFoundException e) {
+		System.out.println("SQL Driver not found!");
+		return;
+	}
+
+	
+	Connection connection = null;
+
+	try {
+		connection = DriverManager
+		.getConnection("jdbc:mysql://localhost:3306/lab3","root", "abbh07@6718");
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery("Select * from accident418;");
+                while(rs.next()){
+                //Retrieve by column name
+                int rn  = rs.getInt("report_number");
+                Date ad = rs.getDate("accd_date");
+                String loc = rs.getString("location");
+               
+
+                //Display values
+                System.out.print("Report Number: " + rn);
+                System.out.print(", Accident Date: " + ad);
+                System.out.print(", Location: " + loc);
+                System.out.println();
+                }
+                
+        }catch (SQLException e) {
+		System.out.println("Connection Failed!");
+	}
+
+	  
+    }
+}
+
---
+++ src/regression/Test_1.java
@@ -0,0 +1,156 @@
+package regression; 
+
+import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import matrix.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aakash
+ */
+public class Test_1 {//database to matrix conversion
+    public static void main(String[] args) throws SQLException, NoSquareException
+    {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+        String s;
+   
+        //connect your app to mysql database  
+        //boolean autoReconnect;
+        //boolean useSSL = false;
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lab3","root", "abbh07@6718");
+      
+      
+        st = con.createStatement();
+        s = "select SA,SB,SC,SD from login.demo";
+        rs = st.executeQuery(s);
+        ResultSetMetaData rsmt = rs.getMetaData();
+        Statement stmt1 = con.createStatement();
+        ResultSet rs1 = stmt1.executeQuery("SELECT COUNT(*) AS total FROM login.demo");
+        int count = 0;
+        while(rs1.next()){
+        count = rs1.getInt("total");
+        }
+        Statement stmt2 = con.createStatement();
+        ResultSet rs2 = stmt2.executeQuery("SELECT Disease FROM login.demo");
+
+        
+
+        String a = "";
+        int nCol = rs.getMetaData().getColumnCount();
+        double ar[][] = new double[count][nCol];
+        //System.out.println(rowNumb);
+        List<int[]> table = new ArrayList<>();
+        while( rs.next()) {
+        int[] row = new int[nCol];
+        for( int iCol = 1; iCol <= nCol; iCol++ ){
+            a = rs.getObject(iCol).toString();
+            row[iCol-1] = Integer.parseInt(a);
+        }
+        table.add( row );
+    }
+
+    int nColy = rs2.getMetaData().getColumnCount();
+    double ary[][] = new double[count][nColy];
+
+List<int[]> tabley = new ArrayList<>();
+while( rs2.next()) {
+    int[] rowy = new int[nColy];
+    for( int iCol = 1; iCol <= nColy; iCol++ ){
+        a = rs2.getObject(iCol).toString();
+        rowy[iCol-1] = Integer.parseInt(a);
+    }
+    tabley.add( rowy );
+}
+
+
+    
+
+/*String b ="";
+List<int[]> table = new ArrayList<>();
+int arr[] = new int[count];
+for(int m=1;m<=count;m++)
+{
+    b = rs2.getObject(m).toString();
+    arr[m-1] = Integer.parseInt(b);
+}
+for(int p = 0;p<count;p++)
+    System.out.println(arr[p]);*/
+
+int i=0;
+int j=0;
+// print result
+for( int[] row: table ){
+    for( int si: row ){
+       ar[i][j]=si;
+       j++;
+    }
+    j=0;
+    i++;
+  
+}
+
+i=0;
+j=0;
+// print result
+for( int[] row: tabley ){
+    for( int si: row ){
+       ary[i][j]=si;
+       j++;
+    }
+    j=0;
+    i++;
+  
+}
+
+
+/*for(int k=0;k<count;k++)
+    {
+        for(int l=0;l<nCol;l++)
+        {
+            System.out.print(ar[k][l]+" ");
+        }
+        System.out.println();
+    }
+for(int k=0;k<count;k++)
+    {
+        for(int l=0;l<nColy;l++)
+        {
+         
+            //Matrix m = new Matrix(ar);
+            System.out.print(ary[k][l]+" ");
+        }
+        System.out.println(); 
+    }*/
+            
+            
+            
+            
+    Matrix m = new Matrix(ar);
+    Matrix m2 = new Matrix(ary);
+    MultiLinear ml  = new MultiLinear(m,m2);
+    Matrix result = ml.calculate(); 
+for(int i1=0;i1<result.getNrows();i1++){
+    for(int j1 = 0;j1<result.getNcols();j1++)
+    {
+        System.out.print(result.getValueAt(i1, j1));
+    }
+    System.out.println();
+}
+//MultiLinear(ar,ary);
+}
+
+}
